c1f90a702997f291e4b401b086b17a840cbc433e
Eddy Navia <201512519@est.umss.edu>
2023-04-04 21:21:45 -0400

    se añadio la funcion de coreccion ortografica y se cambio y se  elimino  el codigo de angular

--- user_stories_check/controladores.js
+++ user_stories_check/controladores.js
@@ -1,4 +1,35 @@
-//pasar a un service :3
+// funcion 1 para corregir ortograficamente las historias de usuario
+
+// Importamos la librería
+const Typo = requirte ('typo-js');
+
+// Creamos un objeto con el diccionario del idioma deseado
+const dictionary = new Typo('es_ES');
+
+// Función para corregir el texto
+function corregirTexto(texto) {
+  // Separamos el texto en palabras
+  const palabras = texto.split(' ');
+  
+  // Recorremos cada palabra y corregimos la ortografía
+  for (let i = 0; i < palabras.length; i++) {
+    // Verificamos si la palabra está mal escrita
+    if (!dictionary.check(palabras[i])) {
+      // Obtenemos sugerencias de corrección
+      const sugerencias = dictionary.suggest(palabras[i]);
+      
+      // Si hay sugerencias, reemplazamos la palabra por la primera sugerencia
+      if (sugerencias.length > 0) {
+        palabras[i] = sugerencias[0];
+      }
+    }
+  }
+  
+  // Unimos las palabras corregidas en un solo texto
+  return palabras.join(' ');
+}
+
+// funcion para crear nuevo documento
 function crearArchivo() {
     let contenido = "";
     let nombreArchivo = prompt("Ingrese el nombre del archivo:");
