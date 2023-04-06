fda020e068fb2619d9281724cf1d39dbc471374f
lopez elvis <201401071@est.umss.edu>
2023-04-06 11:32:45 -0400

    Create SetyGet.js
    Se crea los metodos set y get para la manipulacion del texto requerido, indispensables para la interacion entre clases

---
+++ SetyGet.js
@@ -0,0 +1,18 @@
+
+  
+      document.addEventListener('DOMContentLoaded', function() {
+        class Text {
+          constructor() {
+            this.text = '';
+          }
+          get() {
+    return this.text;
+        }
+
+          set(value) {
+            this.text = value;
+          }
+        }
+         
+    
+      });
